f4c7481e4c17ce2ee843adf2431805fc315fe9ad
Qian Lu <qlu@wpi.edu>
2016-11-25 11:35:40 -0500

    Use data-driven (reactive) forms

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 
-export class SigninComponent {
+export class SigninComponent implements OnInit{
+    signupForm: FormGroup;
 
+    onSubmit() {
+        console.log(this.signupForm);
+        this.signupForm.reset();
+    }
+
+    ngOnInit(): void {
+        this.signupForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
