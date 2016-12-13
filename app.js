4a52067f81c00daee55e35ab8184dee04d404b58
Qian Lu <qlu@wpi.edu>
2016-12-12 23:53:55 -0500

    Wiring up Frontend and Backend to save messages

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messageRoutes = require('./routes/messages');
 
 var app = express();
 
-//mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('localhost:27017/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,11 +1,23 @@
 import { Message } from './message.model';
+import { Http, Response, Headers } from "@angular/http";
+import { Injectable } from "@angular/core";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
 
+@Injectable()
 export class MessageService {
     private messages: Message[] = [];
 
+    // needs Injectable decorator and import HttpModule in app.module
+    constructor(private http: Http) {}
+
     addMessage(message: Message) {
         this.messages.push(message);
-        console.log(this.messages);
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})  // sets up observable object holding request
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     getMessages() {
